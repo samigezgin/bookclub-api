37b6008fd47d6737011151a766c76c723400060b
Sami Gezgin <samigezgin@gmail.com>
2026-01-02 10:55:33 +0300

    test

--- src/main/java/com/bookclub/api/controller/HealthController.java
+++ src/main/java/com/bookclub/api/controller/HealthController.java
@@ -1,16 +1,20 @@
 package com.bookclub.api.controller;
 
 import com.bookclub.api.common.ApiResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/health")
+@Slf4j
 public class HealthController {
 
     @GetMapping
     public ApiResponse<String> health() {
+        log.info("test");
+
         return ApiResponse.success("OK");
     }
 }
