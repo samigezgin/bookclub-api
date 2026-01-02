b13692a61bbdc075bb215b1d84138978cdeedd37
Sami Gezgin <samigezgin@gmail.com>
2026-01-02 15:49:44 +0300

    dto lar oluştu

---
+++ src/main/java/com/bookclub/api/book/controller/BookController.java
@@ -0,0 +1,10 @@
+package com.bookclub.api.book.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/books")
+public class BookController {
+
+}
---
+++ src/main/java/com/bookclub/api/book/dto/BookDto.java
@@ -0,0 +1,15 @@
+package com.bookclub.api.book.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class BookDto {
+    private Long id;
+    private String title;
+    private String author;
+    private String summary;
+}
